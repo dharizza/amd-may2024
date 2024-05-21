0737536df011325b46c9ce304a6b0bd948fe13e8
Dharizza Espinach B. <dharizza@gmail.com>
2024-05-21 20:18:56 +0000

    Show how to override config.

--- project/web/sites/default/settings.php
+++ project/web/sites/default/settings.php
@@ -778,3 +778,10 @@ if (file_exists(__DIR__ . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
+
+# Override configuration properties.
+# Config override for error level:
+# $config['system.logging']['error_level'] = 'verbose';
+# Config override for CSS and JS aggregation:
+# $config['system.performance']['js']['preprocess'] = FALSE;
+# $config['system.performance']['css']['preprocess'] = FALSE;
