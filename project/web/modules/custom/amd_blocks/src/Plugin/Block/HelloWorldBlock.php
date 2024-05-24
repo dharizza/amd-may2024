a40c9866113dba921f81448fe54978ba927e7740
Dharizza Espinach B. <dharizza@gmail.com>
2024-05-24 19:55:35 +0000

    Use currentUser service to print username.

--- project/web/modules/custom/amd_blocks/src/Plugin/Block/HelloWorldBlock.php
+++ project/web/modules/custom/amd_blocks/src/Plugin/Block/HelloWorldBlock.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Drupal\amd_blocks\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\user\Entity\User;
 
 /**
  * Provides an amdblockshelloworld block.
@@ -21,9 +22,13 @@ final class HelloWorldBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build(): array {
+    // For using current user.
+    $user = \Drupal::currentUser()->getAccountName();
+
     $build['content'] = [
-      '#markup' => $this->t('Hello!'),
+      '#markup' => $this->t('Hello @username! Say hi to @another', ['@username' => $user]),
     ];
+
     return $build;
   }
 
